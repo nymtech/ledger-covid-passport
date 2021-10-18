081c1231ac16620cefd35e589db33e72c73880ef
Mark Sinclair <mmsinclair@gmail.com>
2021-10-18 15:39:54 +0100

    Scan verifier code: show list of attributes to be included and excluded from proof

--- packages/ledger-mobile-app/src/routes/User/ViewItem/CovidPass/ScanVerifierCodeConfirm.tsx
+++ packages/ledger-mobile-app/src/routes/User/ViewItem/CovidPass/ScanVerifierCodeConfirm.tsx
@@ -4,7 +4,9 @@ import { Link } from 'react-router-dom';
 import KeyboardArrowLeftIcon from '@material-ui/icons/KeyboardArrowLeft';
 import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
 import CheckCircleOutlineIcon from '@material-ui/icons/CheckCircleOutline';
+import CheckBoxOutlineBlankIcon from '@material-ui/icons/CheckBoxOutlineBlank';
 import ReportGmailerrorredIcon from '@material-ui/icons/ReportGmailerrorred';
+import CheckBoxIcon from '@material-ui/icons/CheckBox';
 import { routes } from '../../../../Routes';
 
 export const CovidPassConfirmVerifierCode: React.FC = () => (
@@ -23,18 +25,26 @@ export const CovidPassConfirmVerifierCode: React.FC = () => (
       <KeyboardArrowLeftIcon /> Back
     </Button>
     <h2>Disclose Information?</h2>
-    <Box mt={2}>Please confirm the information you want to disclose:</Box>
     <Box mt={2}>
-      <FormControlLabel
-        control={<Switch checked />}
-        label="Vaccination Status"
-      />
+      Please confirm that you consent to disclosing the following information:
     </Box>
-    <Box mt={2}>
-      <FormControlLabel control={<Switch />} label="Date of birth" />
+    <Box mt={2} display="flex" alignItems="center">
+      <CheckBoxIcon sx={{ mr: 1 }} /> Vaccination Status
     </Box>
-    <Box mt={2}>
-      <FormControlLabel control={<Switch />} label="Your full name" />
+    <Box mt={2} display="flex" alignItems="center">
+      <CheckBoxIcon sx={{ mr: 1 }} /> Over 18 years old
+    </Box>
+    <Box mt={2} display="flex" alignItems="center">
+      <CheckBoxOutlineBlankIcon sx={{ mr: 1 }} /> Over 21 years old
+    </Box>
+    <Box mt={2} display="flex" alignItems="center">
+      <CheckBoxOutlineBlankIcon sx={{ mr: 1 }} /> Date of birth
+    </Box>
+    <Box mt={2} display="flex" alignItems="center">
+      <CheckBoxOutlineBlankIcon sx={{ mr: 1 }} /> Your full name
+    </Box>
+    <Box mt={2} display="flex" alignItems="center">
+      <CheckBoxOutlineBlankIcon sx={{ mr: 1 }} /> Vaccine brand name and type
     </Box>
     <Grid
       container
