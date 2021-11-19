0796237e263b7cdcf6b26142a555610d2ea8d697
Mark Sinclair <mmsinclair@gmail.com>
2021-11-19 18:28:49 +0000

    Remove `Link` because it renders an anchor that navigates instead of using push state

--- packages/ledger-mobile-app/src/routes/User/App/Nav.tsx
+++ packages/ledger-mobile-app/src/routes/User/App/Nav.tsx
@@ -7,35 +7,38 @@ import {
   useTheme,
 } from '@material-ui/core';
 import QrCodeScannerIcon from '@material-ui/icons/QrCodeScanner';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import AccountBalanceWalletIcon from '@material-ui/icons/AccountBalanceWallet';
 import { routes } from '../../../Routes';
 
 export const AppNav: React.FC = () => {
   const theme: Theme = useTheme();
+  const history = useHistory();
   return (
-    <>
-      <Paper sx={{ width: '100%' }} elevation={3}>
-        <BottomNavigation showLabels>
-          <BottomNavigationAction />
-          <BottomNavigationAction
-            label="Show PCC"
-            icon={<QrCodeScannerIcon />}
-            component={Link}
-            to={routes.user.app.view.covidPass.scanVerifierCode}
-            sx={{
-              background: theme.palette.primary.main,
-              color: theme.palette.common.white,
-            }}
-          />
-          <BottomNavigationAction
-            label="Wallet"
-            icon={<AccountBalanceWalletIcon />}
-            component={Link}
-            to={routes.user.app.home}
-          />
-        </BottomNavigation>
-      </Paper>
-    </>
+    <Paper sx={{ width: '100%' }} elevation={3}>
+      <BottomNavigation showLabels>
+        <BottomNavigationAction />
+        <BottomNavigationAction
+          label="Show PCC"
+          icon={<QrCodeScannerIcon />}
+          onClick={(e) => {
+            e.preventDefault();
+            history.push(routes.user.app.view.covidPass.scanVerifierCode);
+          }}
+          sx={{
+            background: theme.palette.primary.main,
+            color: theme.palette.common.white,
+          }}
+        />
+        <BottomNavigationAction
+          label="Wallet"
+          icon={<AccountBalanceWalletIcon />}
+          onClick={(e) => {
+            e.preventDefault();
+            history.push(routes.user.app.home);
+          }}
+        />
+      </BottomNavigation>
+    </Paper>
   );
 };
